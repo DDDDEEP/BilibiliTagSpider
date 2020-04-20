64ecef84b818a52a4753bea74c12a3a4db4bf457
DDDDEEP <517975369@qq.com>
2020-04-21 00:40:24 +0800

    feat: manage_scrapy_redis.py

--- BilibiliTagWeb/BilibiliTagSpider/spiders/TagRedisSpider.py
+++ BilibiliTagWeb/BilibiliTagSpider/spiders/TagRedisSpider.py
@@ -1,15 +1,13 @@
 import json
-import math
 import time
 import urllib.parse as urlparse
 from urllib.parse import parse_qs
 
-from scrapy import Request
 from scrapy.utils.project import get_project_settings
 from scrapy_redis.spiders import RedisSpider
 
-from ..items import VideoItem, RecordItem
-from helpers import date_to_timestamp, timestamp_to_date, stat_to_int, RecordStatus
+from ..items import VideoItem
+from helpers import stat_to_int
 
 
 class TagRedisSpider(RedisSpider):
@@ -20,130 +18,22 @@ class TagRedisSpider(RedisSpider):
         "bilibili.com",
     ]
 
-    def __init__(self,
-                 *args,
-                 **kwargs):
+    def __init__(self, *args, **kwargs):
         super(TagRedisSpider, self).__init__(*args, **kwargs)
-        self.newlist_url = "https://api.bilibili.com/x/web-interface/newlist?rid={type_id}&type=0&pn={page}&ps={per_page}"
-        self.video_url = "https://www.bilibili.com/video/av{aid}"
-        self.stat_url = "https://api.bilibili.com/x/web-interface/archive/stat?aid={aid}"
-        self.hotlist_url = "https://s.search.bilibili.com/cate/search?main_ver=v3&search_type=video&view_type=hot_rank&order=pubdate&copy_right=-1&"
-        self.hotlist_url += "cate_id={type_id}&page={page}&pagesize={per_page}&time_from={time_from}&time_to={time_to}"
         self.settings = get_project_settings()
-
-        self.items_total = 0  # 需爬取的视频总数
-        self.parse_items_total = {}  # 各日期已爬取的视频总数
-        self.TYPE_ID = 0  # 分区id
-        self.TIME_FROM = 0  # 起始日期
-        self.TIME_TO = 0  # 结束日期
 
     def parse(self, response):
         """
-        通过热度排行API，按日期排序，获取视频总数，然后分日期进行爬取
+        在热度排序API的返回结果中，解析视频数据
         """
         parsed = urlparse.urlparse(response.url)
         param_list = parse_qs(parsed.query)
-        self.TYPE_ID = int(param_list['cate_id'][0])
-        self.TIME_FROM = int(param_list['time_from'][0])
-        self.TIME_TO = int(param_list['time_to'][0])
+        type_id = int(param_list['cate_id'][0])
 
-        self.logger.info("parse_hotlist_init，得到回应：{}".format(response.url))
-        result = json.loads(response.text)
-        if 'numResults' in result and result['numResults'] >= 0:
-            self.items_total = result['numResults']
-            self.logger.info(
-                "[分区号{type_id}，{time_from}-{time_to}]的总视频数为{total}".format(
-                    type_id=self.TYPE_ID,
-                    time_from=self.TIME_FROM,
-                    time_to=self.TIME_TO,
-                    total=result['numResults'],
-                ))
-
-            # 处理得到范围内每日的yyyyMMdd日期，分别进行请求
-            timestamp_time_from = date_to_timestamp(self.TIME_FROM)
-            timestamp_time_to = date_to_timestamp(self.TIME_TO)
-            timestamp_cur = timestamp_time_from
-            while (timestamp_cur <= timestamp_time_to):
-                time_cur = timestamp_to_date(timestamp_cur)
-                yield Request(self.hotlist_url.format(type_id=self.TYPE_ID,
-                                                      page=1,
-                                                      per_page=1,
-                                                      time_from=time_cur,
-                                                      time_to=time_cur),
-                              meta={'time_cur': time_cur},
-                              callback=self.parse_hotlist_one_day,
-                              errback=self.errback_common)
-                timestamp_cur += 86400
-        else:
-            self.logger.error("parse_hotlist_init，热榜分页信息解析失败：{}".format(
-                response.url))
-
-    def parse_hotlist_one_day(self, response):
-        """
-        同一日期下，依次请求热度排行api，按日期排序的各页号的数据，进行爬取
-        """
-        self.logger.info("parse_hotlist_one_day，得到回应：{}".format(response.url))
-        result = json.loads(response.text)
-        if 'numResults' in result and result['numResults'] >= 0:
-            time_cur = response.meta['time_cur']
-            day_total_page = math.ceil(result['numResults'] /
-                                       self.settings['PER_PAGE'])
-            self.parse_items_total[time_cur] = {
-                'cur': 0,
-                'total': result['numResults']
-            }
-
-            if 'time_from_equal_to' in response.meta and response.meta[
-                    'time_from_equal_to']:
-                self.logger.info(
-                    "[分区号{type_id}，{time_from}-{time_to}]的总视频数为{total}".format(
-                        type_id=self.TYPE_ID,
-                        time_from=self.TIME_FROM,
-                        time_to=self.TIME_TO,
-                        total=result['numResults'],
-                    ))
-                self.items_total = result['numResults']
-
-            self.logger.info(
-                "[分区号{type_id}，{time_cur}]的视频数为{total}，分页为每页{per_page}个，共{total_page}页，请求延迟为{delay}s"
-                .format(
-                    type_id=self.TYPE_ID,
-                    time_cur=time_cur,
-                    total=result['numResults'],
-                    per_page=self.settings['PER_PAGE'],
-                    total_page=day_total_page,
-                    delay=self.settings['DOWNLOAD_DELAY'],
-                ))
-
-            # 依次请求对应各页号的数据
-            cur_page = 1
-            while (cur_page <= day_total_page):
-                yield Request(self.hotlist_url.format(
-                    type_id=self.TYPE_ID,
-                    page=cur_page,
-                    per_page=self.settings['PER_PAGE'],
-                    time_from=response.meta['time_cur'],
-                    time_to=response.meta['time_cur'],
-                ),
-                              meta={
-                                  'time_cur': time_cur,
-                              },
-                              callback=self.parse_hotlist_one_day_page,
-                              errback=self.errback_common)
-                cur_page += 1
-        else:
-            self.logger.error("parse_hotlist_one_day，热榜分页信息解析失败：{}".format(
-                response.url))
-
-    def parse_hotlist_one_day_page(self, response):
-        """
-        在热度排序API的返回结果中，解析视频数据
-        """
         self.logger.info("parse_hotlist_one_day_page，得到回应：{}".format(
             response.url))
         result = json.loads(response.text)
         if 'result' in result and 'numPages' in result:
-            time_cur = response.meta['time_cur']
             for video in result['result']:
                 pubdate_arr = time.strptime(video['pubdate'],
                                             '%Y-%m-%d %H:%M:%S')
@@ -152,7 +42,7 @@ class TagRedisSpider(RedisSpider):
                 # TODO:部分stat数据需用另外的接口获取，之后的开发扩展再补充
                 item = VideoItem(
                     aid=video['id'],
-                    tid=self.TYPE_ID,
+                    tid=type_id,
                     title=video['title'],
                     pubdate=pubdate,
                     duration=video['duration'],
@@ -169,39 +59,9 @@ class TagRedisSpider(RedisSpider):
                     stat_dislike=0,
                 )
                 yield item
-                self.parse_items_total[time_cur]['cur'] += 1
-
-            # 记录该日爬取已完成
-            if self.parse_items_total[time_cur][
-                    'cur'] == self.parse_items_total[time_cur]['total']:
-                item = RecordItem(tid=self.TYPE_ID,
-                                  pubdate=date_to_timestamp(
-                                      response.meta['time_cur']),
-                                  status=RecordStatus.Crawled.value)
-                yield item
-                self.logger.info("[分区号{type_id}，{time_cur}]的视频爬取完成".format(
-                    type_id=self.TYPE_ID,
-                    time_cur=time_cur,
-                ))
         else:
             self.logger.error("parse_hotlist_one_day_page，热榜列表解析失败：{}".format(
                 response.url))
-
-    def parse_tag_from_videopage(self, response):
-        """
-        在视频主页中解析标签
-        """
-        tags = response.xpath(
-            r'//body/div[@id="app"]/div[@class="v-wrap"]/div[@class="l-con"]/div[@id="v_tag"]/ul/li[@class="tag"]/a/text()'
-        )
-
-        if len(tags) == 0:
-            self.logger.error("视频主页解析标签失败：{}".format(response.url))
-        else:
-            tags = tags.extract()
-            item = response.meta['item']
-            item['tags'] = ','.join(tags)
-            yield item
 
     def errback_common(self, failure):
         self.logger.error("url请求出错：{}, {}".format(failure.request.url,
---
+++ BilibiliTagWeb/manage_scrapy_redis.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+"""分布式爬虫启动脚本，管理start_urls队列。"""
+
+import argparse
+import math
+import redis
+import requests
+import time
+
+from BilibiliTagSpider import settings
+from helpers import print_time
+
+# 命令行参数
+#     - type_id   -- 分区id
+#     - time-from -- 起始日期（输入格式yyyyMMdd)
+#     - time-to   -- 结束日期
+parser = argparse.ArgumentParser()
+parser.add_argument('--type-id', type=int, default=0)
+parser.add_argument('--time-from', type=str, default='20100101')
+parser.add_argument('--time-to', type=str, default='20100101')
+args = parser.parse_args()
+
+type_id = int(args.type_id)
+time_from = int(args.time_from)
+time_to = int(args.time_to)
+time_start = time.time()
+
+# 请求热度排行api，获取分页数据量
+hotlist_url = "https://s.search.bilibili.com/cate/search?main_ver=v3&search_type=video&view_type=hot_rank&order=pubdate&copy_right=-1&"
+hotlist_url += "cate_id={type_id}&page={page}&pagesize={per_page}&time_from={time_from}&time_to={time_to}"
+response = requests.get(
+    hotlist_url.format(type_id=type_id,
+                       page=1,
+                       per_page=1,
+                       time_from=time_from,
+                       time_to=time_to)).json()
+per_page = settings.PER_PAGE
+pages = math.ceil(response['numResults'] / per_page)
+num_results = response['numResults']
+print(
+    "分区号{type_id}, [{time_from}-{time_to}]共有{num_results}个视频，{pages}页，每页{per_page}个视频"
+    .format(
+        type_id=type_id,
+        time_from=time_from,
+        time_to=time_to,
+        num_results=num_results,
+        pages=pages,
+        per_page=per_page,
+    ))
+
+# 向Redis的start_urls键添加url
+REDIS_START_URL_KEY = 'tag_redis_spider:start_urls'
+REDIS_ITEMS_KEY = 'tag_redis_spider:items'
+conn = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=0,
+    password=settings.REDIS_PARAMS['password'],
+    decode_responses=True  # 设置为True返回的数据格式就是时str类型
+)
+conn.delete(REDIS_START_URL_KEY)
+conn.delete(REDIS_ITEMS_KEY)
+for index in range(1, pages + 1):
+    url = hotlist_url.format(type_id=type_id,
+                             page=index,
+                             per_page=per_page,
+                             time_from=time_from,
+                             time_to=time_to)
+    conn.rpush(REDIS_START_URL_KEY, url)
+
+# 监控爬虫进度
+item_len = conn.llen(REDIS_ITEMS_KEY)
+while item_len != num_results:
+    time.sleep(2)
+    item_len = conn.llen(REDIS_ITEMS_KEY)
+    print("总爬取进度：({}/{})，{:.1f}%".format(
+        item_len,
+        num_results,
+        item_len / num_results * 100,
+    ))
+
+print(
+    "分区号{type_id}, [{time_from}-{time_to}]共有{num_results}个视频，{pages}页，每页{per_page}个视频"
+    .format(
+        type_id=type_id,
+        time_from=time_from,
+        time_to=time_to,
+        num_results=response['numResults'],
+        pages=pages,
+        per_page=per_page,
+    ))
+print_time("爬取所用时间：", time.time() - time_start)
+
+# TODO 插入记录每日视频爬取完成的record项
