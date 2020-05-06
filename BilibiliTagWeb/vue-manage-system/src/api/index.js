ec93b9f6c1adf87b48f389f41b99cdc4fa70a3d1
DDDDEEP <517975369@qq.com>
2020-05-06 11:44:41 +0800

    chore: rm vue-manage-system

--- BilibiliTagWeb/vue-manage-system/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import request from '../utils/request';
-
-export const fetchData = (query) => {
-    return request({
-        url: '/api/video',
-        method: 'get',
-        params: query
-    })
-}
